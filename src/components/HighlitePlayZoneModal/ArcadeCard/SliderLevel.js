5b58d4919487cca9e0e35db4c7d9d883a3b772d3
JahirRayhan572 <jahir.rayhan@bedatasolutions.com>
2024-09-18 20:12:39 +0600

    updated

--- src/components/HighlitePlayZoneModal/ArcadeCard/SliderLevel.js
+++ src/components/HighlitePlayZoneModal/ArcadeCard/SliderLevel.js
@@ -1,36 +1,179 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import svgIcons from "../../../assets/image/SVG/svg";
+const Levels = [
+  "Explorer",
+  "Rookie",
+  "Adventurer",
+  "Master",
+  "Champion",
+  "Legend",
+  "Hero",
+  "Titan",
+  "Native",
+  "Super Native",
+];
 
- const Levels = [
-        "Explorer",
-        "Rookie",
-        "Adventurer",
-        "Master",
-        "Champion",
-        "Legend",
-        "Hero",
-        "Titan",
-        "Native",
-        "Super Native",
-]
-const SliderLevel = () => {
+const typeOfreward={
 
+   artefact:[
 
-        // Explorer: 0 - 99
-        // Rookie: 100 - 999
-        // Adventurer: 1000 - 2499 
-        // Master: 2500 - 4999
-        // Champion: 5000 - 7499
-        // Legend: 7500 - 9999
-        // Hero: 10000 - 12499
-        // Titan: 12500 - 14999
-        // Native: 15000 - 17499
-        // Super Native: 17500+
+   ],
+   reward:[
 
+   ],
+   badges:[
 
-  return (
-    <div>SliderLevel</div>
-  )
+   ]
+
+  
 }
 
-export default SliderLevel
+
+  const rewardsIcon = [
+    svgIcons.referrals,
+    svgIcons.servey,
+    svgIcons.UserGenerateContent,
+    svgIcons.userActivation,
+    svgIcons.userHabit,
+    svgIcons.reviewRestimonial,
+  ];
+
+
+const SliderLevel = () => {
+
+  const [selectedEarning, setSelectedEarning] = useState("artefact");
+
+  const getOrdinalSuffix = (rank) => {
+    if (rank % 10 === 1 && rank % 100 !== 11) {
+      return "st";
+    } else if (rank % 10 === 2 && rank % 100 !== 12) {
+      return "nd";
+    } else if (rank % 10 === 3 && rank % 100 !== 13) {
+      return "rd";
+    } else {
+      return "th";
+    }
+  };
+  
+  const typeOfReward = {
+    artefact: ["Referrals", "Survey", "Content"],
+    reward: [],
+    badges: [],
+  };
+
+  const [current, setCurrent] = useState(0);
+
+  const NeedPoints = 1200;
+
+  const currentLevel = Levels.indexOf("Rookie");
+
+  const handleSelectEarning = (earningType) => {
+    setSelectedEarning(earningType);
+  };
+
+  return (
+    <>
+      <div className="top-part">
+        <p style={{ fontSize: "25px" }}>
+          Hello
+          <br /> <span style={{ fontSize: "45px" }}>Ollie</span>
+        </p>
+        <div
+          style={{ cursor: "pointer" }}
+          dangerouslySetInnerHTML={{ __html: svgIcons.edit }}
+        />
+      </div>
+      <div className="slider-level-container">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          centeredSlides
+          initialSlide={currentLevel}
+        >
+          {Levels.map((level, index) => (
+            <SwiperSlide key={index}>
+              <p
+                style={{ fontWeight: "bold" }}
+                className={`level-text ${
+                  currentLevel === index ? "active-level" : "inactive-level"
+                }`}
+              >
+                {level}
+              </p>
+              {index === currentLevel && <div className="progressbar" />}
+              {index === currentLevel && <div className="circle" />}
+              {index === currentLevel && (
+                <p style={{}} className="nextlevel">
+                  (1200 to level up)
+                </p>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      <div className="point-part">
+        <div>
+          <span>Points</span>
+          <p>1950</p>
+        </div>
+        <div>
+          <span>Streaks</span>
+          <p>1950</p>
+        </div>
+        <div>
+          <span>Rank</span>
+          <p>
+            4<sup>{getOrdinalSuffix(4)}</sup>
+          </p>
+        </div>
+      </div>
+      
+      <div className="quest-name">
+        <p
+          className={selectedEarning === "artefact" ? "active-earning" : ""}
+          onClick={() => handleSelectEarning("artefact")}
+        >
+          Artefacts (28)
+        </p>
+        <p
+          className={selectedEarning === "reward" ? "active-earning" : ""}
+          onClick={() => handleSelectEarning("reward")}
+        >
+          Reward (1)
+        </p>
+        <p
+          className={selectedEarning === "badges" ? "active-earning" : ""}
+          onClick={() => handleSelectEarning("badges")}
+        >
+          Badges (5)
+        </p>
+      </div>
+
+     
+      <div className="rewards-boxes">
+      {typeOfReward[selectedEarning].length > 0 ?(
+        rewardsIcon.map((icon,index)=>(
+          <div key={index} className="reward-point">
+          <div style={{marginTop:'5px'}} dangerouslySetInnerHTML={{ __html: icon }} />
+          <p style={{margin:'0'}}>x 1</p>
+          <p style={{fontSize:'0.5rem',marginBottom:'2px'}}>for sending referrals</p>
+        </div>
+        ))
+      ):(
+       
+        <p>No data available for {selectedEarning}.</p>
+      )}
+
+
+      </div>
+    </>
+  );
+};
+
+export default SliderLevel;
--- src/components/HighlitePlayZoneModal/HighlitePlayZoneModal.js
+++ src/components/HighlitePlayZoneModal/HighlitePlayZoneModal.js
@@ -9,18 +9,7 @@ import SliderLevel from './ArcadeCard/SliderLevel'
 
 const HighlitePlayZoneModal = () => {
 
-  const getOrdinalSuffix = (rank) => {
-    if (rank % 10 === 1 && rank % 100 !== 11) {
-      return 'st'
-    } else if (rank % 10 === 2 && rank % 100 !== 12) {
-      return 'nd'
-    } else if (rank % 10 === 3 && rank % 100 !== 13) {
-      return 'rd'
-    } else {
-      return 'th'
-    }
-  };
-
+ 
   return (
    <div className='highlite-modal-overlay'>
     
@@ -29,40 +18,16 @@ const HighlitePlayZoneModal = () => {
        
     <div className='top-card'
      style={{
-      backgroundImage:'url(https://res.cloudinary.com/pitchspace/image/upload/v1/player-icons/PlayerCharacter,3)',
+      backgroundImage:'url(https://res.cloudinary.com/pitchspace/image/upload/v1/player-icons/mobile/PlayerCharacter,3)',
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       padding:'20px'
     }}
     >
-      <div className='top-part'>
-        <p>Hello<br/> <span style={{fontSize:'25px'}}>Ollie</span></p>
-        <div
-          style={{cursor:'pointer'}}
-          dangerouslySetInnerHTML={{ __html: svgIcons.edit }}
-        />
-      </div>
-
-      {/* slider-level */}
 
        <SliderLevel/>
-
-      {/* point section */}
-      <div className='point-part'>
-        <div>
-          <span>Points</span>
-          <p>1950</p>
-        </div>
-        <div>
-          <span>Streaks</span>
-          <p >1950</p>
-        </div>
-        <div>
-          <span>Rank</span>
-          <p>4<sup>{getOrdinalSuffix(4)}</sup></p>
-        </div>
-      </div>
+      
     </div> 
     
        
